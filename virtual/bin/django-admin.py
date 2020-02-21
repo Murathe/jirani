0d79aa07976d065285f9357348246f7221a4e973
murathe <isaacsm07p@gmail.com>
2020-02-21 15:13:50 +0300

    Create project

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/moringa-school-projects/core/py/Django/jirani/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
