5dbb26863d2bdc894d8d2da38fdad647984c14e8
murathe <isaacsm07p@gmail.com>
2020-02-24 08:14:40 +0300

    Create neighborhood class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    
+
+    class Meta:
+        verbose_name = _("Neighborhood")
+        verbose_name_plural = _("Neigh  borhoods")
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Neighborhood_detail", kwargs={"pk": self.pk})
+    
+    
