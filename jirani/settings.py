54c6598085a836240eae2d989b9bbd84be2fff6c
murathe <isaacsm07p@gmail.com>
2020-02-24 09:08:11 +0300

    cHANGE DB

--- jirani/settings.py
+++ jirani/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'jirani.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresl',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
