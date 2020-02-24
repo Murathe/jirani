b21e2544fb0fc021491c694ba771b6e521ab17ca
murathe <isaacsm07p@gmail.com>
2020-02-24 08:18:38 +0300

    Create neccesary forms - profile, jirani

---
+++ hood/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Profile,Neighborhood,Business,Post
+from django.contrib.auth.models import User
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic', 'bio']
+
+class NeighborhoodForm(forms.ModelForm):
+
+    class Meta:
+        model = Neighborhood
+        fields = [ 'name','location','police','police_department_address','health','health_department_address']
+
+
+class BusinessForm(forms.ModelForm):
+
+    class Meta:
+        model = Business
+        exclude = [ 'business_user','business_neighborhood']
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = [ 'categories','post_image','description']
